501b637261a0c075c0cd47fa947fbdd28890a866
Peter Fan <peter.j.fan@gmail.com>
2022-05-17 16:58:07 -0400

    Added ERC20Integration to Reputation Content page and Moralis to _app. Removed hard-coded env variables for now.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,15 @@ import '../../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Web3ContextProvider } from '@/context/web3Context';
 import { StyledEngineProvider } from '@mui/material';
+import { MoralisProvider } from "react-moralis";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <StyledEngineProvider injectFirst>
       <Web3ContextProvider>
-        <Component {...pageProps} />
+        <MoralisProvider serverUrl="" appId="">
+          <Component {...pageProps} />
+        </MoralisProvider>
       </Web3ContextProvider>
     </StyledEngineProvider>
   );
