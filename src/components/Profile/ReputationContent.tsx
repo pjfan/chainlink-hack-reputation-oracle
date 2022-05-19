190cb3235da61431e21bc274f70cfab3c5952096
Peter Fan <pjfan@users.noreply.github.com>
2022-05-19 15:42:49 -0400

    Erc20 integration (#3)
    * added moralis sdk to yarn
    
    * Added useWalletBalance hook
    
    * Created ERC20Integration component
    
    * Removed header for ERC20 Integration
    
    * Added antd React library to make use of table
    
    * Added ERC20Integration to Reputation Content page and Moralis to _app. Removed hard-coded env variables for now.

--- src/components/Profile/ReputationContent.tsx
+++ src/components/Profile/ReputationContent.tsx
@@ -1,11 +1,13 @@
 import { Box, Grid } from '@mui/material';
 import React from 'react';
+import { ERC20Integration } from './ERC20Integration';
+import { useWeb3 } from '@/context/web3Context';
 
 export const ReputationContent: React.FC = () => {
   return (
     <Grid container spacing={2}>
       <Grid item xs={6} md={4}>
-        <Box sx={{ width: 200, height: 200, backgroundColor: 'primary.main' }}>POAP integration</Box>
+        <Box sx={{ width: 200, height: 200, backgroundColor: 'primary.main' }}>ERC20 integration<ERC20Integration chain="" address=""></ERC20Integration></Box>
       </Grid>
 
       <Grid item xs={6} md={4}>
