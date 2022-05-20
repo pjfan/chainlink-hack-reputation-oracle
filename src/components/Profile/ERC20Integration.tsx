4a8a0ed2bc07591936c80eb9fe9d9b2bcd8b261a
Victor Fei <vf63@cornell.edu>
2022-05-20 12:55:01 -0700

    Use .env for Moralis setup (#4)
    * added .env for moralis
    
    * removed debugger statement

--- src/components/Profile/ERC20Integration.tsx
+++ src/components/Profile/ERC20Integration.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Skeleton, Table } from "antd";
+import { Skeleton, Table } from 'antd';
 import { MoralisChainOptions, useWalletBalance, WalletBalance } from '../../hooks/useWalletBalance';
 
 export interface ERC20IntegrationProps {
-    address: string;
-    chain: MoralisChainOptions;
+  address: string;
+  chain: MoralisChainOptions;
 }
 
 export const ERC20Integration: React.FC<ERC20IntegrationProps> = (props: ERC20IntegrationProps) => {
@@ -12,12 +12,12 @@ export const ERC20Integration: React.FC<ERC20IntegrationProps> = (props: ERC20In
 
   const columns: object[] = [
     {
-      title: "",
-      dataIndex: "logo",
-      key: "logo",
+      title: '',
+      dataIndex: 'logo',
+      key: 'logo',
       render: (logo: string) => (
         <img
-          src={logo || "https://etherscan.io/images/main/empty-token.png"}
+          src={logo || 'https://etherscan.io/images/main/empty-token.png'}
           alt="nologo"
           width="28px"
           height="28px"
@@ -25,55 +25,52 @@ export const ERC20Integration: React.FC<ERC20IntegrationProps> = (props: ERC20In
       ),
     },
     {
-      title: "Symbol",
-      dataIndex: "symbol",
-      key: "symbol",
+      title: 'Symbol',
+      dataIndex: 'symbol',
+      key: 'symbol',
       render: (symbol: string) => symbol,
     },
     {
-      title: "Name",
-      dataIndex: "name",
-      key: "name",
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
       render: (name: string) => name,
     },
     {
-      title: "Balance",
-      dataIndex: "balance",
-      key: "balance",
+      title: 'Balance',
+      dataIndex: 'balance',
+      key: 'balance',
       render: (balance: number) => balance,
     },
     {
-      title: "Price",
-      dataIndex: "price",
-      key: "price",
+      title: 'Price',
+      dataIndex: 'price',
+      key: 'price',
       render: (price: number) => price,
     },
     {
-      title: "Value",
-      dataIndex: "value",
-      key: "value",
+      title: 'Value',
+      dataIndex: 'value',
+      key: 'value',
       render: (value: number) => value,
     },
     {
-      title: "Address",
-      dataIndex: "token_address",
-      key: "token_address",
+      title: 'Address',
+      dataIndex: 'token_address',
+      key: 'token_address',
       render: (address: string) => address,
     },
   ];
 
   return (
-    <div>
-      <Skeleton loading={!assets}>
-        <Table
-          dataSource={assets!}
-          columns={columns}
-          rowKey={(record) => {
-            return record.token_address;
-          }}
-        />
-      </Skeleton>
-    </div>
+    <Skeleton loading={!assets}>
+      <Table
+        dataSource={assets!}
+        columns={columns}
+        rowKey={(record) => {
+          return record.token_address;
+        }}
+      />
+    </Skeleton>
   );
 };
-
--- src/hooks/useWalletBalance.ts
+++ src/hooks/useWalletBalance.ts
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react";
-import { useMoralis, useMoralisWeb3Api } from "react-moralis";
+import { useEffect, useState } from 'react';
+import { useMoralis, useMoralisWeb3Api } from 'react-moralis';
 
 export interface WalletBalance {
   symbol: string;
@@ -8,7 +8,7 @@ export interface WalletBalance {
   price?: number;
   value?: number;
   token_address: string;
-};
+}
 
 export interface TokenBalance {
   token_address: string;
@@ -20,51 +20,93 @@ export interface TokenBalance {
   balance: string;
 }
 
-export type MoralisChainOptions = "eth" | "0x1" | "ropsten" | "0x3" | "rinkeby" | "0x4" | "goerli" | "0x5" | "kovan" | "0x2a" | "polygon" | "0x89" | "mumbai" | "0x13881" | "bsc" | "0x38" | "bsc testnet" | "0x61" | "avalanche" | "0xa86a" | "avalanche testnet" | "0xa869" | "fantom" | "0xfa" | undefined;
+export type MoralisChainOptions =
+  | 'eth'
+  | '0x1'
+  | 'ropsten'
+  | '0x3'
+  | 'rinkeby'
+  | '0x4'
+  | 'goerli'
+  | '0x5'
+  | 'kovan'
+  | '0x2a'
+  | 'polygon'
+  | '0x89'
+  | 'mumbai'
+  | '0x13881'
+  | 'bsc'
+  | '0x38'
+  | 'bsc testnet'
+  | '0x61'
+  | 'avalanche'
+  | '0xa86a'
+  | 'avalanche testnet'
+  | '0xa869'
+  | 'fantom'
+  | '0xfa'
+  | undefined;
 
-export const useWalletBalance = (address: string, chain: MoralisChainOptions): WalletBalance[] | null | undefined => {
+export const useWalletBalance = (
+  address: string,
+  chain: MoralisChainOptions
+): WalletBalance[] | null | undefined => {
   const { account, token } = useMoralisWeb3Api();
   const { isInitialized } = useMoralis();
-  
-  const [assets, setAssets] = useState<WalletBalance[]|null|undefined>(undefined);
-  
+
+  const [assets, setAssets] = useState<WalletBalance[] | null | undefined>(undefined);
+
   useEffect(() => {
     if (isInitialized) {
       getTokenBalances().then((balance) => setAssets(balance));
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isInitialized]);
-  
-  const getTokenPrice = async (inputToken: TokenBalance, chain: MoralisChainOptions): Promise<number | null> => {
-    // slow down API call rate.
-    await new Promise(resolve => setTimeout(resolve, 1200));
-    console.log("Fetching price data for: " + inputToken.name);
-    const price: number | null = await token.getTokenPrice({"address": inputToken.token_address, "chain": chain}).then((token) => token.usdPrice).catch((err) => {console.log(err); return null;});
-    return price
-  }
 
-  const getTokenBalances = async ():Promise<WalletBalance[] | null> => {
+  const getTokenPrice = async (
+    inputToken: TokenBalance,
+    chain: MoralisChainOptions
+  ): Promise<number | null> => {
+    // slow down API call rate.
+    await new Promise((resolve) => setTimeout(resolve, 1200));
+    console.log('Fetching price data for: ' + inputToken.name);
+    const price: number | null = await token
+      .getTokenPrice({ address: inputToken.token_address, chain: chain })
+      .then((token) => token.usdPrice)
+      .catch((err) => {
+        console.log(err);
+        return null;
+      });
+    return price;
+  };
+
+  const getTokenBalances = async (): Promise<WalletBalance[] | null> => {
     // fetch token balance from address
-    const tokens: TokenBalance[] | null = await account.getTokenBalances({"address": address, "chain": chain}).catch((err) => {console.log(err); return null;});
-    console.log("Fetched all ERC20 balances for chain: " + chain);
-  
+    const tokens: TokenBalance[] | null = await account
+      .getTokenBalances({ address: address, chain: chain })
+      .catch((err) => {
+        console.log(err);
+        return null;
+      });
+    console.log('Fetched all ERC20 balances for chain: ' + chain);
+
     // create Map() to store token balances, populate with info from API call.
     const tokenBalances: Map<String, WalletBalance> = new Map();
     for (const token of tokens!) {
       const tokenInfo: WalletBalance = {
-        "name": token.name, 
-        "symbol": token.symbol, 
-        "balance": parseFloat(token.balance) * Math.pow(10, (-1*parseFloat(token.decimals))), 
-        "price": undefined, 
-        "value": undefined,
-        "token_address": token.token_address
+        name: token.name,
+        symbol: token.symbol,
+        balance: parseFloat(token.balance) * Math.pow(10, -1 * parseFloat(token.decimals)),
+        price: undefined,
+        value: undefined,
+        token_address: token.token_address,
       };
       tokenBalances.set(token.token_address, tokenInfo);
     }
-    
+
     // attempt to get price data for each token
     for (const token of tokens!) {
-      const price: number | null  = await getTokenPrice(token, chain);
+      const price: number | null = await getTokenPrice(token, chain);
       try {
         const tokenInfo: WalletBalance | undefined = tokenBalances.get(token.token_address);
         if (tokenInfo && price) {
@@ -72,14 +114,13 @@ export const useWalletBalance = (address: string, chain: MoralisChainOptions): W
           tokenInfo!.value = tokenInfo.price * tokenInfo.balance;
           tokenBalances.set(token.token_address, tokenInfo);
         }
-      }
-      catch(error){
+      } catch (error) {
         console.log(error);
       }
     }
 
     return Array.from(tokenBalances.values());
-  }
+  };
 
   return assets;
 };
